f82b3bde2f4b1bfc874163298803d76081f61bc1
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2023-08-27 18:47:58 +0300

    feature: mirror hearts effect implemented

--- app/src/main/java/ru/heatalways/amazingasfuckapplication/presentation/common/composables/Heart.kt
+++ app/src/main/java/ru/heatalways/amazingasfuckapplication/presentation/common/composables/Heart.kt
@@ -1,14 +1,23 @@
 package ru.heatalways.amazingasfuckapplication.presentation.common.composables
 
+import android.graphics.BlurMaskFilter
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -17,68 +26,116 @@ import ru.heatalways.amazingasfuckapplication.presentation.styles.Sizes
 
 @Composable
 fun Heart(
+    modifier: Modifier = Modifier,
     color: Color = Color.Transparent,
     strokeColor: Color = AppTheme.colors.primary,
     strokeWidth: Dp = Sizes.HeartStrokeWidth,
-    modifier: Modifier = Modifier,
+    blurRadius: Float = 30f,
 ) {
     val path = Path()
+    val lightingPaint = Paint()
+    val frameworkPaint = lightingPaint.asFrameworkPaint()
+    frameworkPaint.maskFilter = BlurMaskFilter(blurRadius, BlurMaskFilter.Blur.NORMAL)
+    frameworkPaint.color = strokeColor.toArgb()
+    frameworkPaint.style = android.graphics.Paint.Style.STROKE
+    frameworkPaint.strokeWidth = with(LocalDensity.current) { strokeWidth.toPx() }
 
     Canvas(
         modifier = modifier
     ) {
-        val strokeWidthPx = strokeWidth.toPx()
-
-        path.apply {
-            val width: Float = size.width
-            val height: Float = size.height
-
-            // Starting point
-            moveTo(width / 2, height / 5)
-
-            // Upper left path
-            cubicTo(
-                5 * width / 14, 0f,
-                0f, 0f,
-                width / 28, 2 * height / 5
-            )
-
-            // Lower left path
-            cubicTo(
-                width / 14, 2 * height / 3,
-                3 * width / 7, 5 * height / 6,
-                width / 2, height - strokeWidthPx
-            )
-
-            // Lower right path
-            cubicTo(
-                4 * width / 7, 5 * height / 6,
-                13 * width / 14, 2 * height / 3,
-                27 * width / 28, 2 * height / 5
-            )
-
-            // Upper right path
-            cubicTo(
-                width, 0f,
-                9 * width / 14, 0f,
-                width / 2, height / 5
-            )
-        }
-
-        drawPath(
-            path = path,
+        drawHeart(
+            x = 0f,
+            y = 0f,
+            width = size.width,
+            height = size.height,
             color = color,
-            style = Fill
+            strokeColor = strokeColor,
+            strokeWidth = strokeWidth,
+            path = path,
+            lightingPaint = lightingPaint,
+        )
+    }
+}
+
+fun DrawScope.drawHeart(
+    x: Float,
+    y: Float,
+    width: Float,
+    height: Float,
+    rotation: Float = 0f,
+    color: Color = Color.Transparent,
+    strokeColor: Color = Color.Red,
+    strokeWidth: Dp = Sizes.HeartStrokeWidth,
+    path: Path,
+    lightingPaint: Paint,
+) {
+    val strokeWidthPx = strokeWidth.toPx()
+
+    path.apply {
+        reset()
+
+        // Starting point
+        moveTo(width / 2, height / 5)
+
+        // Upper left path
+        cubicTo(
+            5 * width / 14, 0f,
+            0f, 0f,
+            width / 28, 2 * height / 5
         )
 
-        drawPath(
-            path = path,
-            color = strokeColor,
-            style = Stroke(
-                width = strokeWidthPx,
-                cap = StrokeCap.Round,
-            )
+        // Lower left path
+        cubicTo(
+            width / 14, 2 * height / 3,
+            3 * width / 7, 5 * height / 6,
+            width / 2, height - strokeWidthPx
         )
+
+        // Lower right path
+        cubicTo(
+            4 * width / 7, 5 * height / 6,
+            13 * width / 14, 2 * height / 3,
+            27 * width / 28, 2 * height / 5
+        )
+
+        // Upper right path
+        cubicTo(
+            width, 0f,
+            9 * width / 14, 0f,
+            width / 2, height / 5
+        )
+    }
+
+    rotate(
+        degrees = rotation,
+        pivot = Offset(x + width / 2, y + height / 2)
+    ) {
+        translate(
+            left = x,
+            top = y,
+        ) {
+            drawPath(
+                path = path,
+                color = color,
+                style = Fill
+            )
+
+            drawPath(
+                path = path,
+                color = strokeColor,
+                style = Stroke(
+                    width = strokeWidthPx,
+                    cap = StrokeCap.Round,
+                )
+            )
+
+            drawIntoCanvas { canvas ->
+                canvas.drawPath(
+                    path = path,
+                    paint = lightingPaint,
+                )
+            }
+        }
     }
 }
 
--- app/src/main/java/ru/heatalways/amazingasfuckapplication/presentation/screens/mirror/MirrorScreen.kt
+++ app/src/main/java/ru/heatalways/amazingasfuckapplication/presentation/screens/mirror/MirrorScreen.kt
@@ -7,21 +7,34 @@ import android.content.Context
 import androidx.camera.core.CameraSelector
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -35,13 +48,23 @@ import com.google.accompanist.permissions.rememberPermissionState
 import org.koin.androidx.compose.koinViewModel
 import ru.heatalways.amazingasfuckapplication.R
 import ru.heatalways.amazingasfuckapplication.presentation.common.composables.AppBar
+import ru.heatalways.amazingasfuckapplication.presentation.common.composables.Heart
+import ru.heatalways.amazingasfuckapplication.presentation.common.composables.radialBackgroundLighting
 import ru.heatalways.amazingasfuckapplication.presentation.common.navigation.api.ScreenRoute
 import ru.heatalways.amazingasfuckapplication.presentation.screens.mirror.MirrorContract.Intent
 import ru.heatalways.amazingasfuckapplication.presentation.styles.AppTheme
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
+import kotlin.math.roundToInt
 
 object MirrorScreenRoute : ScreenRoute()
+
+private const val HEARTS_COUNT = 20
+private const val HEART_MIN_SIZE_DP = 30
+private const val HEART_MAX_SIZE_DP = 100
+private const val HEART_MIN_ROTATION = -45
+private const val HEART_MAX_ROTATION = 45
+private const val HEART_ANIMATION_DURATION = 5000
 
 @Composable
 fun MirrorScreen(viewModel: MirrorViewModel = koinViewModel()) {
@@ -52,37 +75,38 @@ fun MirrorScreen(viewModel: MirrorViewModel = koinViewModel()) {
         permission = Manifest.permission.CAMERA
     )
 
-    if (cameraPermissionState.status.isGranted) {
-        val previewView: PreviewView = remember { PreviewView(context) }
-        val preview = androidx.camera.core.Preview.Builder().build()
+    val preview = androidx.camera.core.Preview.Builder().build()
 
-        preview.setSurfaceProvider(previewView.surfaceProvider)
+    val cameraSelector: MutableState<CameraSelector> = remember {
+        mutableStateOf(CameraSelector.DEFAULT_FRONT_CAMERA)
+    }
 
-        val cameraSelector: MutableState<CameraSelector> = remember {
-            mutableStateOf(CameraSelector.DEFAULT_FRONT_CAMERA)
-        }
-
-        LaunchedEffect(previewView) {
-            val cameraProvider = suspendCoroutine { continuation ->
-                ProcessCameraProvider.getInstance(context).also { future ->
-                    future.addListener(
-                        { continuation.resume(future.get()) },
-                        ContextCompat.getMainExecutor(context)
-                    )
-                }
+    LaunchedEffect(Unit) {
+        val cameraProvider = suspendCoroutine { continuation ->
+            ProcessCameraProvider.getInstance(context).also { future ->
+                future.addListener(
+                    { continuation.resume(future.get()) },
+                    ContextCompat.getMainExecutor(context)
+                )
             }
-
-            cameraProvider.unbindAll()
-
-            cameraProvider.bindToLifecycle(
-                lifecycleOwner,
-                cameraSelector.value,
-                preview,
-            )
         }
 
+        cameraProvider.unbindAll()
+
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            cameraSelector.value,
+            preview,
+        )
+    }
+
+    if (cameraPermissionState.status.isGranted) {
         MirrorPreview(
-            previewFactory = { previewView },
+            previewFactory = {
+                val previewView = PreviewView(context)
+                preview.setSurfaceProvider(previewView.surfaceProvider)
+                previewView
+            },
             onIntent = viewModel::intent,
         )
     } else {
@@ -97,6 +121,9 @@ private fun MirrorPreview(
     previewFactory: (Context) -> PreviewView,
     onIntent: (Intent) -> Unit
 ) {
+    var screenWidth by remember { mutableIntStateOf(0) }
+    var screenHeight by remember { mutableIntStateOf(0) }
+
     Scaffold(
         topBar = {
             AppBar(
@@ -106,13 +133,57 @@ private fun MirrorPreview(
                 onGoBackClick = { onIntent(Intent.OnNavigationButtonClick) },
                 modifier = Modifier.fillMaxWidth()
             )
-        }
+        },
+        modifier = Modifier
+            .onGloballyPositioned {
+                screenWidth = it.size.width
+                screenHeight = it.size.height
+            }
     ) { contentPadding ->
         AndroidView(
             factory = previewFactory,
             modifier = Modifier
                 .fillMaxSize()
         )
+
+        repeat(HEARTS_COUNT) { index ->
+            if (screenHeight == 0 || screenWidth == 0) {
+                return@repeat
+            }
+
+            val rotation = (HEART_MIN_ROTATION..HEART_MAX_ROTATION).random().toFloat()
+            val heartSize = (HEART_MIN_SIZE_DP..HEART_MAX_SIZE_DP).random().dp
+            val heartSizePx = with(LocalDensity.current) { heartSize.toPx() }
+
+            val catLoaderTransition = rememberInfiniteTransition(label = "HeartTransition-$index")
+
+            val x = (0..screenWidth - heartSizePx.roundToInt()).random().toFloat()
+
+            val y by catLoaderTransition.animateFloat(
+                initialValue = screenHeight + heartSizePx,
+                targetValue = -heartSizePx,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = HEART_ANIMATION_DURATION,
+                        delayMillis = (0..HEART_ANIMATION_DURATION).random(),
+                        easing = LinearEasing
+                    ),
+                    repeatMode = RepeatMode.Restart
+                ),
+                label = "translation-$index"
+            )
+
+            Heart(
+                strokeColor = AppTheme.colors.primary,
+                modifier = Modifier
+                    .size(width = heartSize, height = heartSize)
+                    .graphicsLayer {
+                        translationX = x
+                        translationY = y
+                        rotationZ = rotation
+                    }
+            )
+        }
 
         Box(
             modifier = Modifier
