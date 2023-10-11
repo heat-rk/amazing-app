fc92dacbc3fc3996f68d9a5fbc0d0576f21db49c
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2023-10-11 16:01:21 +0300

    refactor: modularization step 6 - removed unnecessary dependencies from app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,9 +3,6 @@ plugins {
     id(AppPlugins.androidKotlin)
     id(AppPlugins.kotlinKapt)
     id(AppPlugins.detekt) version AppPlugins.Versions.detekt
-    id(AppPlugins.parcelize)
-
-    kotlin(AppPlugins.serialization) version AppPlugins.Versions.serialization
 }
 
 android {
@@ -80,15 +77,11 @@ dependencies {
     implementation(project(":core:design"))
     implementation(project(":core:navigation:api"))
     implementation(project(":core:navigation:compose-impl"))
-    implementation(project(":core:composables:heart"))
-    implementation(project(":core:composables:image-cropper"))
-    implementation(project(":core:composables:pager"))
     implementation(project(":core:coroutines:dispatchers"))
     implementation(project(":core:coroutines:scopes"))
     implementation(project(":core:data:utils"))
     implementation(project(":core:data:translator"))
     implementation(project(":core:data:db"))
-    implementation(project(":core:data:cache"))
     implementation(project(":core:data:temp-files"))
     implementation(project(":core:data:http-client"))
     implementation(project(":feature:sharing:api"))
@@ -111,19 +104,16 @@ dependencies {
 
     implementation(AppDependencies.immutableCollections)
     implementation(AppDependencies.Ktx.allImplementations)
-    implementation(AppDependencies.Compose.allImplementations)
+
     implementation(AppDependencies.Coroutines.allImplementations)
     implementation(AppDependencies.Koin.allImplementations)
     implementation(AppDependencies.Accompanist.allImplementations)
-    implementation(AppDependencies.Ktor.allImplementations)
-    implementation(AppDependencies.Mlkit.translate)
-    implementation(AppDependencies.htmlUnit)
-    implementation(AppDependencies.Coil.allImplementations)
-    implementation(AppDependencies.Camera.allImplementations)
-    implementation(AppDependencies.Room.allImplementations)
     implementation(AppDependencies.Orbit.allImplementations)
-
-    kapt(AppDependencies.Room.compiler)
+    implementation(AppDependencies.Coil.allImplementations)
+    implementation(AppDependencies.Compose.material)
+    implementation(AppDependencies.Compose.lifeCycleRuntime)
+    implementation(AppDependencies.Compose.activity)
+    implementation(AppDependencies.Compose.navigation)
 
     debugImplementation(AppDependencies.Compose.debugImplementations)
 }
