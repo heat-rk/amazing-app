ebd0e00493dab869fbfe1228b2c29b77e6205f16
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2023-10-20 10:49:06 +0300

    refactor: image cropped bounds made internal

--- core/composables/image-cropper/src/main/java/ru/heatalways/amazingapplication/core/composables/image_cropper/ImageCroppedBounds.kt
+++ core/composables/image-cropper/src/main/java/ru/heatalways/amazingapplication/core/composables/image_cropper/ImageCroppedBounds.kt
@@ -3,7 +3,7 @@ package ru.heatalways.amazingapplication.core.composables.image_cropper
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 
-sealed interface ImageCroppedBounds {
+internal sealed interface ImageCroppedBounds {
     object NotCalculated : ImageCroppedBounds
 
     data class Calculated(
